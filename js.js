85a50a3aaa35cc3b24820d2837b543ad88837a6f
unknown <melilaral@gmail.com>
2019-12-19 10:54:56 +0100

    Primera Version : Proyecto Busqueda

---
+++ js.js
@@ -0,0 +1,35 @@
+function Buscar(){
+
+    if(document.getElementById("palabra").value==""){
+        document.getElementById("mensaje").value="Debes escribir una palabra a buscar";
+    }
+   else{
+        var palabra = document.getElementById("palabra").value;
+
+        if(palabra=="azul"){
+            document.getElementById("mensaje").value="Palabra encontrada";
+
+        } else if(palabra=="rojo"){
+            document.getElementById("mensaje").value="Palabra encontrada";
+
+        } else if(palabra=="verde"){
+            document.getElementById("mensaje").value="Palabra encontrada";
+
+        } 
+        else{
+            document.getElementById("mensaje").value=" Palabra NO encontrada";
+        }
+
+
+   }
+     
+     
+
+
+}
+
+
+
+function salir(){
+    window.close();
+}
